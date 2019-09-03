b552ecad6522f00dbdbd582773e5ac30f7afc1cf
Sam Caldwell <mail@samcaldwell.net>
2019-09-03 07:47:15 -0700

    adding a successful test using the bootstrap noop mode to ensure we didn't end up with a corrupted executable.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,14 @@
         echo "Missing Dockerfile"
         exit 1
     }
-    docker build --tag contained:local .
+    docker build --compress --tag contained:local . || {
+        echo "contained:local build failed."
+        exit 2
+    }
+    [[ "$(docker run -i contained:local /bin/sh -c 'bootstrap noop')" == 'OK' ]] || {
+        echo "contained:local post build noop test failed."
+        exit 3
+    }
+    echo "contained:local build successful."
+    exit 0
 )
